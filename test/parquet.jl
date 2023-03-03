77d5ebc0dbd99aed92a78c99cd7a721decb1dee2
nwamsley1 <n.t.wamsley@wustl.edu>
2023-03-03 13:26:11 -0600

    fixed getHits()

--- test/parquet.jl
+++ test/parquet.jl
@@ -14,33 +14,37 @@ function getSub(mean::Float64, ppm::Float64, array::Arrow.Primitive{Union{Missin
 end
 
 #Function that given a sorted list of transitions gets all the hits
-
-function getHits(mass_list::Vector{Float32}, ppm::Float64, masses::Vector{Union{Missing, Float32}}, intensities::Vector{Union{Missing, Float32}})
-    MzFeatures = map(x->(low=x-(x/1000000.0)*ppm, mass = x, high=x+(x/1000000.0)*ppm), mass_list);
+#of_eltype(Float32, table.intensities[1]))
+#function getHits(mass_list::Vector{Float32}, ppm::Float64, masses::MappedArray{Float32, 1, Vector{Union{Missing, Float32}}, MappedArrays.var"#7#9"{Float32}, MappedArrays.var"#8#10"{Union{Missing, Float32}}}, 
+#    intensities::MappedArray{Float32, 1, Vector{Union{Missing, Float32}}, MappedArrays.var"#7#9"{Float32}, MappedArrays.var"#8#10"{Union{Missing, Float32}}})
+function getHits(mass_list::Vector{Float32}, ppm::Float32, masses::Vector{Union{Missing, Float32}}, intensities::Vector{Union{Missing, Float32}})
+    function getMzFeatures(mass_list::Vector{Float32}, ppm::Float32)
+       # [(low=x-(x/1000000.0)*ppm, mass = x, high=x+(x/1000000.0)*ppm) for x in mass_list]
+        #[(low=150, mass = 151, high=152), (low=893, mass = 894, high=895)]
+        (map(x->(low=x-(x/1000000.0)*ppm, mass = x, high=x+(x/1000000.0)*ppm, test = ppm*10.0, testb = ppm*20.0), mass_list))
+    end
+    MzFeatures = getMzFeatures(mass_list, ppm)
     feature = 1
     peak = 1
     #println("test");
-    while (peak<=length(masses)) & (feature <=length(mass_list))
-        #println(masses[peak])
+    while (peak <= length(masses)) & (feature <= length(mass_list))
         if masses[peak] > MzFeatures[feature].low
             if masses[peak] < MzFeatures[feature].high
-                println(masses[peak])
-                println(MzFeatures[feature])
                 #"masses[peak] is in the range(low, high), "
                 #There could be multiple peaks in the tolerance and we want to 
                 #choose the one that is closest in mass
-                diff = abs(masses[peak] - MzFeatures[feature].mass)
+                smallest_diff = abs(masses[peak] - MzFeatures[feature].mass)
                 i = 0
                 @inbounds while (masses[peak+1+i] < MzFeatures[feature].high)
-                    new_diff = abs(masses[peak+1+i] - MzFeatures[feature].mass) 
-                    if new_diff < diff
-                        diff = new_diff
+                    new_diff = abs(masses[peak+1+i] - MzFeatures[feature].mass)
+                    if new_diff < smallest_diff
+                        smallest_diff = new_diff
                         peak = peak+1+i
                         i = 0
                     end
                     i+=1
                 end
-                println(MzFeatures[feature].mass, " has mass ", masses[peak], " and intensity ", intensities[peak])
+                #println(MzFeatures[feature].mass, " has mass ", masses[peak], " and intensity ", intensities[peak])
                 feature += 1
                 continue
             end
@@ -51,11 +55,17 @@ function getHits(mass_list::Vector{Float32}, ppm::Float64, masses::Vector{Union{
         #println(peak)
         #println(feature)
     end
-    println(feature)
-    println(peak)
 end
-getHits([151.67221f0, 894.0938f0], 20.0, [151.67221f0, missing, 894.0937f0, 894.0938f0, 894.0939f0], 
-                                        [Float32(100.0), missing, Float32(100.0), missing ,missing])
+
+
+test_masses =  Vector{Union{Missing, Float32}}(
+    [151.67221f0, missing, 894.0937f0, 894.0938f0, 894.0939f0]
+    )
+test_intensities =  Vector{Union{Missing, Float32}}([missing for x in test_masses])
+test_ppm = Float32(20.0)
+test_mz = Vector{Float32}([151.67221f0, 700.0, 894.0938f0])
+
+getHits(test_mz, test_ppm, test_masses, test_intensities)
 #import Base: <
 #function <(a::Any, b::Missing)
 #    false
