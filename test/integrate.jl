c26a60478f8f82f04a06c9ef60dc23d8c59d787a
nwamsley1 <n.t.wamsley@wustl.edu>
2023-03-04 17:33:17 -0600

    added tests for getScanCycleUnion and getIntegrationBounds

--- test/integrate.jl
+++ test/integrate.jl
@@ -23,6 +23,17 @@ NRF2_Survey = Arrow.Table("../data/parquet/Nrf2_SQ_052122_survey.arrow")
     @test getSub(Float32(300.0), [200.0, 300.0, 300.1, 300.00001, 10000.0], ppm = Float32(0)) == []
     @test getSub(Float32(300.0), [300.0], ppm = Float32(20.0)) == [1]
 
+
+    ########
+    #getScanAdresses
+    ########
+
+    #msOrder is a list of scan orders (MS1, MS2, MS3, etc.)
+    #We want a list of "scan adresses" from a list of scan orders
+    #A scan adress records the each cycle (defined by an MS1 scan)
+    #and each MSN scan is numbered starting from 1 within a cycle
+    #See the example below 
+
     test_msOrder = [1, 2, 2, 2, 1, 2, 1, 1, 2, 2];
     test_adresses = Vector{NamedTuple{(:scan_index, :ms1, :msn), Tuple{Int64, Int64, Int64}}}([
      (scan_index = 1, ms1 = 1, msn = 0),
@@ -38,14 +49,46 @@ NRF2_Survey = Arrow.Table("../data/parquet/Nrf2_SQ_052122_survey.arrow")
 
     @test getScanAdresses(test_msOrder) == test_adresses
 
+
     lightVGVNGFGR = getMZ(Precursor(getResidues("VGVNGFGR"), UInt8(2)))
     heavyVGVNGFGR = getMZ(Precursor(getResidues("VGVNGFGR[+10.008269]"), UInt8(2)))
 
+    ########
+    #getScanCycleUnion
+    ########
 
+    test_adresses_a = Vector{NamedTuple{(:scan_index, :ms1, :msn), Tuple{Int64, Int64, Int64}}}([
+        (scan_index = 3, ms1 = 1, msn = 2), # 1
+        (scan_index = 4, ms1 = 1, msn = 3), # 1
+        (scan_index = 6, ms1 = 2, msn = 1), #Also in list 2
+        (scan_index = 7, ms1 = 3, msn = 1), #Cycle 3 in other list
+        (scan_index = 9, ms1 = 5, msn = 1)]);
 
+    test_adresses_b = Vector{NamedTuple{(:scan_index, :ms1, :msn), Tuple{Int64, Int64, Int64}}}([
+        (scan_index = 2, ms1 = 1, msn = 1),
+        (scan_index = 6, ms1 = 2, msn = 1), #2
+        (scan_index = 8, ms1 = 4, msn = 1), #Cycle 4 not in other list
+        (scan_index = 9, ms1 = 4, msn = 10),
+        (scan_index = 10, ms1 = 5, msn = 20)]);
 
-    test_indices_a = Vector{Int64}([1, 2, 10, 11, 12, 13, 60, 61, 62, 63, 64, 65, 66, 67, 68, 100])
-    @test getIntegrationBounds(test_indices_a, max_gap_size = 10) == (7, 15)
+    @test getScanCycleUnion(test_adresses_a, test_adresses_b) == [1, 2, 5]
+    #Given two lists of can adresses, we want to know which MS cycles
+    #have at least one scan from both lists. Then we want to keep those
+    #scans and discard the rest.
+
+    ########
+    #getIntegrationBounds
+    ########
+
+    #Given a list of integers the maximum length subarray
+    #such that each element is one greater than the one before it 
+    #starting with the second element in the subarray. 
+    #A maximum gap size should be allowed. If an element in the 
+    #array is exactly one greater than the previous element, call
+    #it an anchor. The maximum difference between two anchors must
+    #not exceed the maximum gap size for the subarray to be valid
+    #See the examples below
+
     test_indices_a = Vector{Int64}([1, 2, 3, 12, 13])
     @test getIntegrationBounds(test_indices_a, max_gap_size = 10) == (1, 5)
     test_indices_a = Vector{Int64}([1, 2, 3, 12, 100])
@@ -58,5 +101,11 @@ NRF2_Survey = Arrow.Table("../data/parquet/Nrf2_SQ_052122_survey.arrow")
     @test getIntegrationBounds(test_indices_a, max_gap_size = 10) == (1, 1)
     test_indices_a = Vector{Int64}([1,2])
     @test getIntegrationBounds(test_indices_a, max_gap_size = 10) == (1, 2)
+    test_indices_a = Vector{Int64}([1, 2, 10, 11, 12, 13, 60, 61, 62, 63, 64, 65, 66, 67, 68, 100])
+    @test getIntegrationBounds(test_indices_a, max_gap_size = 10) == (7, 15)
+
+    ########
+    #getIntegrationBounds
+    ########
 
 end
