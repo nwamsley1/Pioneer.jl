c7c22b5e57586495f20b426153f078adbda434de
nwamsley1 <n.t.wamsley@wustl.edu>
2023-03-25 14:19:45 -0500

    finished tests for FastXTandem and passing

--- src/PSM_TYPES/FastXTandem.jl
+++ src/PSM_TYPES/FastXTandem.jl
@@ -5,13 +5,11 @@ mutable struct FastXTandem <: PSM
     b_int::Float32
     y_count::Int64
     y_int::Float32
-    last_y::UInt8
-    y_start::UInt8
-    longest_y::Int8
+    y_ions::Set{UInt8}
     error::Float64
 end
 
-FastXTandem() = FastXTandem(0, Float32(0), 0, Float32(0), UInt8(0), UInt8(0), UInt8(0), Float64(0))
+FastXTandem() = FastXTandem(0, Float32(0), 0, Float32(0), Set(UInt8[]), Float64(0))
 results = UnorderedDictionary{UInt32, FastXTandem}()
 #Base.zero(::Type{FragmentMatch}) = FragmentMatch()
 
@@ -35,17 +33,7 @@ function ModifyFeatures!(score::FastXTandem, transition::Transition, mass::Union
     elseif getIonType(transition) == 'y'
         score.y_count += 1
         score.y_int += intensity
-        if score.longest_y == 0
-            score.y_start = getInd(transition)
-            score.last_y = getInd(transition) - 1
-        end
-        if (getInd(transition)-score.last_y) == 1
-            if (getInd(transition) - score.y_start) > score.longest_y
-                score.longest_y = getInd(transition) - score.y_start
-            else
-                score.y_start = getInd(transition)
-            end
-        end
+        push!(score.y_ions, getInd(transition))
     end
     score.error += abs(mass - getMZ(transition))
     #push!(results[prec_id].intensities, (intensities[best_peak]))
@@ -76,9 +64,43 @@ function Score!(PSMs_dict::Dict, unscored_PSMs::UnorderedDictionary{UInt32, Fast
         max(log(score.y_int*score.b_int), 0)
         )
     end
+
+    #Get length of the the longest streak of consecutive y-ions
+    function getLongestY(y_ions_set::Set{UInt8})
+        #Must sort y-ions by index
+        #Might not be the most efficient. 
+        y_ions = sort(collect(y_ions_set));
+
+        #No calculation needed 
+        if length(y_ions) <= 1
+            return length(y_ions)
+        end
+
+        y_streak = 1
+        longest_y_streak = 1
+        for i in 2:length(y_ions)
+            #If this isn't true, the sequence/ladder
+            #is broken and needs to start over. 
+            if (y_ions[i] - y_ions[i-1]) == 1
+                y_streak += 1
+            else
+                #Need to check if this is the longest
+                #streak before resetting the counter
+                if y_streak > longest_y_streak
+                    longest_y_streak = y_streak
+                end
+                y_streak = 1
+            end
+        end
+        if y_streak > longest_y_streak
+            return y_streak
+        end
+        return longest_y_streak
+    end
+
     for key in keys(unscored_PSMs)
         append!(PSMs_dict[:hyperscore], HyperScore(unscored_PSMs[key]))
         append!(PSMs_dict[:error], unscored_PSMs[key].error)
-        append!(PSMs_dict[:y_ladder], unscored_PSMs[key].longest_y)
+        append!(PSMs_dict[:y_ladder], getLongestY(unscored_PSMs[key].y_ions))
     end
 end
--- test/PSM_TYPES/FastXTandem.jl
+++ test/PSM_TYPES/FastXTandem.jl
@@ -26,5 +26,93 @@ end
     ScoreFragmentMatches!(unscored_PSMs, test_matches)
     scored_PSMs = makePSMsDict(FastXTandem())
     Score!(scored_PSMs, unscored_PSMs)
-    scored_PSMs
+
+    #No matched y ions so should be zero
+    @test scored_PSMs[:y_ladder][1] == 0
+    #log(3!) + log(0!) + log(3000*0) = log(6) + 1 = 1.791... 
+    @test Tol(scored_PSMs[:hyperscore][1], 1.79189)
+    #abs(324.15536f0 -  324.15636f0)*2 + abs(538.2871f0 - 538.27739f0) = 0.01171875f0
+    @test Tol(scored_PSMs[:error][1], 0.01171875)
+
+    test_t = sort!(getTransitions(Precursor("PEPTIDE")), by = transition->getMZ(transition))
+    #=8-element Vector{Transition}:
+ Transition(MzFeature(324.15536f0, 324.1489f0, 324.16183f0), 0x00000000, 'b', 0x03, 0x01, 0x00)
+ Transition(MzFeature(376.17142f0, 376.16388f0, 376.17896f0), 0x00000000, 'y', 0x03, 0x01, 0x00)
+ Transition(MzFeature(425.20306f0, 425.19455f0, 425.21158f0), 0x00000000, 'b', 0x04, 0x01, 0x00)
+ Transition(MzFeature(477.2191f0, 477.20953f0, 477.22864f0), 0x00000000, 'y', 0x04, 0x01, 0x00)
+ Transition(MzFeature(538.2871f0, 538.27637f0, 538.29785f0), 0x00000000, 'b', 0x05, 0x01, 0x00)
+ Transition(MzFeature(574.2718f0, 574.2603f0, 574.28326f0), 0x00000000, 'y', 0x05, 0x01, 0x00)
+ Transition(MzFeature(653.314f0, 653.30096f0, 653.3271f0), 0x00000000, 'b', 0x06, 0x01, 0x00)
+ Transition(MzFeature(703.3144f0, 703.30035f0, 703.3284f0), 0x00000000, 'y', 0x06, 0x01, 0x00)
+    =#
+    #Should have one peak to match every transition 
+    test_masses = Vector{Union{Missing, Float32}}([324.15536f0, 376.17142f0, 425.20306f0, 477.2191f0, 
+    538.2871f0, 574.2718f0, 653.314f0, 703.3144f0])
+    test_intensities = Vector{Union{Missing, Float32}}(map(x->1000, test_masses))
+    test_matches = Vector{FragmentMatch}()
+    matchPeaks!(test_matches, test_t, test_masses, test_intensities, 0.0)
+    unscored_PSMs = UnorderedDictionary{UInt32, FastXTandem}()
+    ScoreFragmentMatches!(unscored_PSMs, test_matches)
+    scored_PSMs = makePSMsDict(FastXTandem())
+    Score!(scored_PSMs, unscored_PSMs)
+
+    #No matched y ions so should be zero
+    @test scored_PSMs[:y_ladder][1] == 4
+    #log(4!) + log(4!) + log(4000*4000) = 22.944206940899946
+    @test Tol(scored_PSMs[:hyperscore][1], 22.944206940899946)
+    #masses were exact so should be about zero. 
+    @test Tol(scored_PSMs[:error][1], 0)
+
+    #Make sure y-ladder counting still works when there is a gap
+    test_t = sort!(getTransitions(Precursor("PEPTIDE"))[[1, 2, 3, 4, 5, 7, 8]], by = transition->getMZ(transition))
+    #=7-element Vector{Transition}:
+7-element Vector{Transition}:
+ Transition(MzFeature(324.15536f0, 324.1489f0, 324.16183f0), 0x00000000, 'b', 0x03, 0x01, 0x00)
+ Transition(MzFeature(376.17142f0, 376.16388f0, 376.17896f0), 0x00000000, 'y', 0x03, 0x01, 0x00)
+ Transition(MzFeature(425.20306f0, 425.19455f0, 425.21158f0), 0x00000000, 'b', 0x04, 0x01, 0x00)
+ Transition(MzFeature(538.2871f0, 538.27637f0, 538.29785f0), 0x00000000, 'b', 0x05, 0x01, 0x00)
+ Transition(MzFeature(574.2718f0, 574.2603f0, 574.28326f0), 0x00000000, 'y', 0x05, 0x01, 0x00)
+ Transition(MzFeature(653.314f0, 653.30096f0, 653.3271f0), 0x00000000, 'b', 0x06, 0x01, 0x00)
+ Transition(MzFeature(703.3144f0, 703.30035f0, 703.3284f0), 0x00000000, 'y', 0x06, 0x01, 0x00)
+    =#
+    #Should have one peak to match every transition 
+    test_masses = Vector{Union{Missing, Float32}}([324.15536f0, 376.17142f0, 425.20306f0, 
+    538.2871f0, 574.2718f0, 653.314f0, 703.3144f0])
+    test_intensities = Vector{Union{Missing, Float32}}(map(x->1000, test_masses))
+    test_matches = Vector{FragmentMatch}()
+    matchPeaks!(test_matches, test_t, test_masses, test_intensities, 0.0)
+    unscored_PSMs = UnorderedDictionary{UInt32, FastXTandem}()
+    ScoreFragmentMatches!(unscored_PSMs, test_matches)
+    scored_PSMs = makePSMsDict(FastXTandem())
+    Score!(scored_PSMs, unscored_PSMs)
+
+    @test scored_PSMs[:y_ladder][1] == 2
+    #log(4!) + log(3!) + log(4000*3000) = 21.270230507328275
+    @test Tol(scored_PSMs[:hyperscore][1], 21.270230507328275)
+    #masses were exact so should be about zero. 
+    @test Tol(scored_PSMs[:error][1], 0)
+
+
+    #When charge state is 2, need to make sure we are counting the Y-ladder correctly
+    #and not double counting +1 and +2 charge states or isotopes.  
+    #So if we see the y4+1 and the y4+2 ion, we should only add one to the count of y-ions,
+    #but we should add the intensities of both the y4+1 and y4+2 ions to the y intensity sum. 
+    #Likewise, if we see both +0 and +1 isotopes for y4+1, we should sum both intensities but only
+    #add one to the count of y-ions. 
+    test_t = sort!(getTransitions(Precursor("PEPTIDE"), UInt8[1,2], UInt8[0,1]), by = transition->getMZ(transition))
+    test_masses = Vector{Union{Missing, Float32}}(map(x->getMZ(x), test_t))
+    test_intensities = Vector{Union{Missing, Float32}}(map(x->1000, test_masses))
+    test_matches = Vector{FragmentMatch}()
+    matchPeaks!(test_matches, test_t, test_masses, test_intensities, 0.0)
+    unscored_PSMs = UnorderedDictionary{UInt32, FastXTandem}()
+    ScoreFragmentMatches!(unscored_PSMs, test_matches)
+    scored_PSMs = makePSMsDict(FastXTandem())
+    Score!(scored_PSMs, unscored_PSMs)
+
+    #should hit all 4 y ions
+    @test scored_PSMs[:y_ladder][1] == 4
+    #log(16!) + log(16!) + log(16000*16000) = 80.70440821460518
+    @test Tol(scored_PSMs[:hyperscore][1], 80.70440821460518)
+    #masses were exact so should be about zero. 
+    @test Tol(scored_PSMs[:error][1], 0)
 end
