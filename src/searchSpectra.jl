cb5d69e0ad8fc4198b0d6f03999b7a99032a5794
nwamsley1 <n.t.wamsley@wustl.edu>
2023-04-02 23:55:56 -0500

    cleaning searchSpectra.jl

--- src/searchSpectra.jl
+++ src/searchSpectra.jl
@@ -254,31 +254,49 @@ getMatchedPrecursors(matched_precursors, matches, NRF2_Survey, 0.15)
 
 testmatchp(matches, test_dict)
 
-function plotSpectra!(p::Plots.Plot{Plots.GRBackend}, masses, intensities)
-    for (peak_idx, mass) in enumerate(masses)
-        plot!(p, [mass, mass], [0, intensities[peak_idx]], legend = false, color = "black")
+
+function plotBestSpectra(matched_ions::NamedTuple{(:scan_idx, :name, :mz, :intensity), Tuple{Int64, Vector{String}, Vector{Float32}, Vector{Float32}}}, RAW::Arrow.Table, title::String, out_path::String)
+
+    function plotSpectra!(p::Plots.Plot{Plots.GRBackend}, masses, intensities)
+        for (peak_idx, mass) in enumerate(masses)
+            plot!(p, [mass, mass], [0, intensities[peak_idx]], legend = false, color = "black")
+        end
     end
-end
-#p = plot()
-#plotSpectra(p, NRF2_Survey.masses[2703], NRF2_Survey.intensities[2703])
-#display(p)
-function addFragmentIons!(p::Plots.Plot{Plots.GRBackend}, matched_ions::NamedTuple{(:scan_idx, :name, :mz, :intensity), Tuple{Int64, Vector{String}, Vector{Float32}, Vector{Float32}}})
-    i = 1
-    for (mz, intensity) in zip(matched_ions[:mz], matched_ions[:intensity])
-        plot!(p, [mz, mz], [0, -1*intensity], legend = false, color = "red")
-        annotate!(mz + 1, -1*intensity, fontfamily = "helvetica", text(matched_ions[:name][i], :black, :top, :left, 8))
-        i += 1
+    #p = plot()
+    #plotSpectra(p, NRF2_Survey.masses[2703], NRF2_Survey.intensities[2703])
+    #display(p)
+    function addFragmentIons!(p::Plots.Plot{Plots.GRBackend}, matched_ions::NamedTuple{(:scan_idx, :name, :mz, :intensity), Tuple{Int64, Vector{String}, Vector{Float32}, Vector{Float32}}})
+        i = 1
+        for (mz, intensity) in zip(matched_ions[:mz], matched_ions[:intensity])
+            plot!(p, [mz, mz], [0, -1*intensity], legend = false, color = "red")
+            annotate!(mz + 1, -1*intensity, fontfamily = "helvetica", text(matched_ions[:name][i], :black, :top, :left, 8))
+            i += 1
+        end
     end
-end
-function plotBestSpectra(matched_ions, RAW)
-    p = plot()
+
+    p = plot(title = title, fontfamily="helvetica")
     plotSpectra!(p, RAW[:masses][matched_ions[:scan_idx]], RAW[:intensities][matched_ions[:scan_idx]])
     addFragmentIons!(p, matched_ions)
-    display(p)
-end
-plotBestSpectra(matched_precursors[1].best_psm, NRF2_Survey)
+    savefig(joinpath(out_path,title*".pdf"))
 
-testmatchp(matches, test_dict)
+end
+
+function plotAllBestSpectra(matched_precursors::UnorderedDictionary{UInt32, MatchedPrecursor}, ptable::PrecursorTable, MS_TABLE::Arrow.Table, out_path::String, fname::String)
+    if !isdir(out_path)
+        mkpath(out_path)
+    end
+
+    @time for key in keys(matched_precursors)
+        plotBestSpectra(matched_precursors[key].best_psm, MS_TABLE,
+                        ptable.id_to_pep[getPepIDFromPrecID(ptable, key)].sequence,
+                        out_path)
+    end
+
+    files = filter(x -> isfile(joinpath(out_path, x)) && match(r"\.pdf$", x) != nothing, readdir(out_path))
+    merge_pdfs(map(file -> joinpath(out_path,file), files), joinpath(out_path, fname), cleanup=true)
+
+end
+plotAllBestSpectra(matched_precursors, testPtable, NRF2_Survey, "./data/figures/spectra/", "MERGED_NRF2_SURVEY_SPECTRA.pdf")
 
 
 #testPtable.id_to_pep[1].sequence
@@ -304,7 +322,7 @@ function plotAllFragmentIonChromatograms(matched_precursors::UnorderedDictionary
     files = filter(x -> isfile(joinpath(out_path, x)) && match(r"\.pdf$", x) != nothing, readdir(out_path))
     merge_pdfs(map(file -> joinpath(out_path,file), files), joinpath(out_path, fname), cleanup=true)
 end
-plotAllFragmentIonChromatograms(matched_precursors, testPtable, "./data/figures/", "MERGED_NRF2_SURVEY.pdf")
+plotAllFragmentIonChromatograms(matched_precursors, testPtable, "./data/figures/", "MERGED_NRF2_SURVEY_CHROMATOGRAMS.pdf")
 
 using Glob
 getPepIDFromPrecIDlegend = false
