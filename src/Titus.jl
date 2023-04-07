ae9a45708c745c828165ba85658b7cdf830e284f
nwamsley1 <n.t.wamsley@wustl.edu>
2023-04-07 09:42:10 -0500

    test

--- src/Titus.jl
+++ src/Titus.jl
@@ -1,6 +1,11 @@
 module Titus
 
 include("precursor.jl")
+include("SearchRaw.jl")
+include("matchpeaks.jl")
+include("integrate.jl")
+include("getPRecursors.jl")
+include("binaryRangeQuery.jl")
 # Write your package code here.
 
 end
