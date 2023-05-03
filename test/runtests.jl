631a53b0181ead62ca951da6c1f95549061003a9
nwamsley1 <n.t.wamsley@wustl.edu>
2023-05-03 10:17:40 -0500

    trying to fix CI bug in runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -8,28 +8,35 @@ include("precursor.jl")
 include("../src/applyMods.jl")
 include("applyMods.jl")
 include("../src/Routines/PRM/IS-PRM-SURVEY/buildPrecursorTable.jl")
-include("./Routines/PRM/IS-PRM-SURVEY/buildPrecursorTable.jl")
+#include("./Routines/PRM/IS-PRM-SURVEY/buildPrecursorTable.jl")
+include("Routines/PRM/IS-PRM-SURVEY/buildPrecursorTable.jl")
 
 include("../src/Routines/PRM/IS-PRM/buildPrecursorTable.jl")
-include("./Routines/PRM/IS-PRM/buildPrecursorTable.jl")
+#include("./Routines/PRM/IS-PRM/buildPrecursorTable.jl")
+include("Routines/PRM/IS-PRM/buildPrecursorTable.jl")
+
 include("matchpeaks.jl")
 include("binaryRangeQuery.jl")
 include("PSM_TYPES/FastXTandem.jl")
 
 include("../src/Routines/PRM/IS-PRM-SURVEY/getBestPSMs.jl")
-include("./Routines/PRM/IS-PRM-SURVEY/getBestPSMs.jl")
+#include("./Routines/PRM/IS-PRM-SURVEY/getBestPSMs.jl")
+include("Routines/PRM/IS-PRM-SURVEY/getBestPSMs.jl")
 
 include("../src/Routines/PRM/getMS1PeakHeights.jl")
-include("./Routines/PRM/getMS1PeakHeights.jl")
+#include("./Routines/PRM/getMS1PeakHeights.jl")
+include("Routines/PRM/getMS1PeakHeights.jl")
 
 include("../src/Routines/PRM/IS-PRM-SURVEY/getBestTransitions.jl")
-include("./Routines/PRM/IS-PRM-SURVEY/getBestTransitions.jl")
+#include("./Routines/PRM/IS-PRM-SURVEY/getBestTransitions.jl")
+include("Routines/PRM/IS-PRM-SURVEY/getBestTransitions.jl")
 
 include("../src/Routines/PRM/IS-PRM/getIntegrationBounds.jl")
-include("./Routines/PRM/IS-PRM/getIntegrationBounds.jl")
+#include("./Routines/PRM/IS-PRM/getIntegrationBounds.jl")
+include("Routines/PRM/IS-PRM/getIntegrationBounds.jl")
 
 include("../src/LFQ.jl")
-include("./LFQ.jl")
+include("LFQ.jl")
 
 #include("./PSM_TYPES/FastXTandem.jl")
 #include("getPrecursors.jl")
