9ba0c8366ff5d0227e793f98ad52e2c427e860cb
nwamsley1 <n.t.wamsley@wustl.edu>
2023-09-02 12:44:37 -0500

    added test data for fitting

--- src/Routines/LibrarySearch/huberlossScratch.jl
+++ src/Routines/LibrarySearch/huberlossScratch.jl
@@ -5,29 +5,29 @@
 @load "/Users/n.t.wamsley/Desktop/Hs_test.jld2" Hs
 @load "/Users/n.t.wamsley/Desktop/IDtoROW.jld2" IDtoROW
 δ = 10000.0
-δ = 10000.0
+#δ = 1000000.0
 weights = sparseNMF(Hs, X, zero(Float32), zero(Float32));
 b = X[:];
 X₁ = 1000*ones(eltype(Hs), Hs.n);
-X₀ = 1000*ones(eltype(Hs), Hs.n);
 r = Hs*X₁ .- b
-function solveHuber2!(Hs::SparseMatrixCSC{Float32, Int64}, r::Vector{T}, X₀::Vector{T}, X₁::Vector{T}, δ::T; max_iter::Int = 1000, tol::AbstractFloat = 100.0, λ::Float32 = zero(Float32)) where {T<:AbstractFloat}
+#=function solveHuber2!(Hs::SparseMatrixCSC{Float32, Int64}, r::Vector{T}, X₁::Vector{T}, δ::T; max_iter_outer::Int = 1000, max_iter_inner::Int = 20, tol::U = 100, λ::Float32 = zero(Float32)) where {T<:AbstractFloat,U<:Real}
     L1 = zero(T)
     L2 = zero(T)
-    ΔX = Inf
+    ΔX = T(Inf)
     i = 0
-    E = zeros(T, max_iter)
-    while (ΔX > tol) & (i < max_iter)
-        #ΔX = 0.0
+    while (i < max_iter_outer) & (ΔX > tol)
+        #Sum of differences bewteen i+1 and i'th iteration
+        ΔX = 0.0
+        #Loop through single variable optimizations
         for col in range(1, Hs.n)
             L1,L2 = zero(T), zero(T)
             n = 0
             X0 = X₁[col]
-
+            X₀ = X0
             ########
-            #Newton-Raphson to optimize w.r.t X_col. 
+            #Newton-Raphson to optimize w.r.t X₁[col]. 
             ########
-            while n < 20
+            while (n < max_iter_inner) & (abs((X₁[col]-X0)/X0) > 0.1/i)
                 X0 = X₁[col]
                 L1 = zero(T)
                 L2 = zero(T)
@@ -38,16 +38,13 @@ function solveHuber2!(Hs::SparseMatrixCSC{Float32, Int64}, r::Vector{T}, X₀::V
                     #L2 += Hs.nzval[i]^2
 
                     #Huber
-                    #R = 1 + (r[Hs.rowval[i]]/δ)^2
-                    #L1 += Hs.nzval[i]*r[Hs.rowval[i]]*(R^(-1/2))
-                    #L2 += (Hs.nzval[i]^2)*( R^(-1/2) - (r[Hs.rowval[i]]/δ)^2*(R^(-3/2)) )
-                    
                     R = (r[Hs.rowval[i]]/δ)^2
                     L1 += Hs.nzval[i]*r[Hs.rowval[i]]*((1 + R)^(-1/2))
                     L2 += (Hs.nzval[i]^2)/((1 + R)^(3/2))
                 end
-                #X₁[col] = max(X₁[col] - (L1/L2), 0.0)
                 X₁[col] = max(X₁[col] - (L1+λ)/L2, 0.0)
+
+                #Update residuals 
                 @turbo for i in Hs.colptr[col]:(Hs.colptr[col + 1] - 1)
                     r[Hs.rowval[i]] += Hs.nzval[i]*(X₁[col] - X0)
                 end
@@ -55,40 +52,33 @@ function solveHuber2!(Hs::SparseMatrixCSC{Float32, Int64}, r::Vector{T}, X₀::V
                 ########
                 #Stopping Criterion for single variable Newton-Raphson
                 #Accuracy requirement increases each outer-loop
+                #Need to find a generally acceptable parameter value
                 ########
                 if abs((X₁[col]-X0)/X0) < 0.1/i
                     break
                 end
                 n += 1
             end
-            #println(n)
-            #X₁[col] = max(X₁[col], 0.0)
-            #ΔX += abs(X₁[col] - X₀[col])
+            ΔX += abs(X₁[col]-X₀)
         end
-
-
-        ###########
-        #Stopping Criterion
-        ###########
-        E[i + 1] = sum(abs.(X₁ .- X₀))
-        if E[i + 1] < Hs.n*1
-            println("stop at i = $i")
-            return E
-        end
-        
-        ###########
-        #Reset X₁ and X₀
-        ###########
-        @turbo for col in eachindex(X₁)
-            X₀[col] = X₁[col]
-        end
-
         i += 1
     end
     println(i)
-    return E
+    return
+end=#
+include("src/ML/sparseNNLS.jl")
+@time begin 
+    for i in range(1, 1000)
+        solveHuber2!(Hs, r, X₁, Float32(δ), max_iter_outer = 10, max_iter_inner = 20, tol = Hs.n*10, λ = Float32(0.0));
+        X₁.=Float32(1000)
+        r = Hs*X₁ .- b
+    end
 end
-E = solveHuber2!(Hs, r, X₀, X₁, Float32(δ), max_iter = 100, λ = Float32(0.0));
+solveHuber2!(Hs, r, X₁, Float32(δ), max_iter_outer = 100, max_iter_inner = 20, tol = Hs.n*10, λ = Float32(0.0))
+X₁ = X₁ = 100*ones(eltype(Hs), Hs.n);
+r = Hs*X₁ .- b;
+solveHuber2!(Hs, r, X₁, Float32(δ), max_iter_outer = 10, max_iter_inner = 20, tol = Hs.n*10, λ = Float32(0.0))
+
 dot(X₁, weights[:])/(norm(X₁)*norm(weights[:]))
 corspearman(X₁, weights[:])
 dot(X₁.==0.0, weights[:].==0.0)/(norm(X₁.==0.0)*norm(weights[:].==0.0))
