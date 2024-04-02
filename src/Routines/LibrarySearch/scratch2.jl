a49a58fdfad53e0e1bde156d5f882388af44beb7
nwamsley1 <n.t.wamsley@wustl.edu>
2024-04-02 16:48:30 -0500

    working on tournament tree k-way merge

--- src/Routines/LibrarySearch/scratch2.jl
+++ src/Routines/LibrarySearch/scratch2.jl
@@ -4,9 +4,10 @@ sorted_sub_ranges = Vector{UnitRange{UInt32}}([1:10, 11:15, 12:12, 95:100, 85:87
 for sub_range in sorted_sub_ranges
     sort!(@view(test_values[sub_range]))
 end
+include("src/Routines/LibrarySearch/scratch3.jl")
 test_ttree = TournamentTree(
     Vector{TTreeNode{UInt32, Float32}}(undef, 1),
-    Vector{Leaf{UInt32, Float32}}(undef, 2)
+    one(UInt32)
 )
 
 buildTTree!(
@@ -15,3 +16,14 @@ buildTTree!(
     UInt32(length( sorted_sub_ranges)),
     test_values
 )
+n = 1
+max_elements = sum([(last(x) - first(x) + 1) for x in sorted_sub_ranges])
+while n < max_elements 
+removeSmallestElement!(
+    test_ttree,
+    test_values,
+    sorted_out,
+    n,
+)
+n += 1
+end
--- src/Routines/LibrarySearch/scratch3.jl
+++ src/Routines/LibrarySearch/scratch3.jl
@@ -13,20 +13,23 @@ end
 
 mutable struct TournamentTree{I<:Unsigned,T<:Real}
     nodes::Vector{TTreeNode{I,T}}
+    n_bins::I
 end
-getNode(ttree::TournamentTree, node_id::Int64) = ttree.nodes[node_id]
-setNode!(ttree::TournamentTree, node::Node, node_id::Int64) = ttree.nodes[node_id] = node
+getNode(ttree::TournamentTree, node_id::UInt32) = ttree.nodes[node_id]
+getNodes(ttree::TournamentTree) = ttree.nodes
+setNode!(ttree::TournamentTree, node::TTreeNode, node_id::UInt32) = ttree.nodes[node_id] = node
 
 
-function fillNode!(ttree::TournamentTree{I,T}, node_id::Int64, bin_id::I, top_id::I, bin_len::I) where {I<:Unsigned, T<:Real}
+function fillNode!(ttree::TournamentTree{I,T}, node_id::UInt32, val::T, bin_id::I, top_id::I, bin_len::I) where {I<:Unsigned, T<:Real}
     setNode!(ttree, 
                 TTreeNode(val, bin_id, top_id, bin_len),
                 node_id
     )
 end
 
-function growTTree!(ttree::TournamentTree{I,T}, N::Integer) where {I<:Unsigned, T<:Real}
+function growTTree!(ttree::TournamentTree{I,T}, N::I) where {I<:Unsigned, T<:Real}
     ttree.nodes = Vector{TTreeNode{I, T}}(undef, N + N - 1)
+    ttree.n_bins = N
 end
 
 function getNearestPowerOf2(n::UInt32)
@@ -50,16 +53,17 @@ function buildTTree!(
 
     #Number of leaves needs to be the next highest power of 2. 
     N = UInt32(getNearestPowerOf2(n_sub_bins))
-    if N > length(getLeaves(ttree))
+    if N+N-1 > length(getNodes(ttree))
         growTTree!(ttree, N)
     end
-
+    println("N $N")
     #Fill leaves 
     i = one(UInt32)
     for sub_bin_range in sub_bin_ranges
         top_id = first(sub_bin_range)
         last_id = last(sub_bin_range)
         fillNode!(ttree, 
+                    i, #node_id
                     values[first(sub_bin_range)], #val
                     i, #bin_id
                     top_id,
@@ -67,8 +71,9 @@ function buildTTree!(
                     )
         i += one(UInt32)
     end
-    for i in range(n_sub_bins, N) #Fill placeholder leaves (if last_sub_bin_idx is not a power of 2)
+    for i in range(n_sub_bins+one(UInt32), N) #Fill placeholder leaves (if last_sub_bin_idx is not a power of 2)
         fillNode!(ttree, 
+                i,
                 typemax(T),
                 i, #leaf_id
                 one(I),
@@ -76,14 +81,13 @@ function buildTTree!(
         )
     end
     #Fill remaining nodes 
-    n = N >> 1 #Number of nodes in base layer of the tree 
-    start = node_id
     lower_node_id = 1
-    upper_node_id = n + 1
-    n = n >> 1
+    upper_node_id = N + 1
+    n = N >> 1
     while n > 1#node_id <= (N - 1) #While there are nodes left to fill
         start = upper_node_id
-        while upper_node_id <= start + n #Fill current layer of nodes 
+        n = n >> 1
+        while upper_node_id <= start + n + 1 #Fill current layer of nodes 
 
             node_a, node_b = ttree.nodes[lower_node_id], ttree.nodes[lower_node_id+1]
 
@@ -95,35 +99,64 @@ function buildTTree!(
             lower_node_id += 2
             upper_node_id += 1
         end
-        n = n >> 1
     end
 
 end
 
-function removeSmallestElement(
+function removeSmallestElement!(
     ttree::TournamentTree{I, T},
     values_in::Vector{T},
     values_out::Vector{T},
-    out_idx::Int64,
-    leading_node_id::Int64
+    out_idx::Int64
 ) where {I<:Unsigned,T<:Real}
-    node = getNode(ttree, leading_node_id)
+    #Number of bins in the merge 
+    n = ttree.n_bins
+    #Get node containing minimum value 
+    node_id = n + n - one(UInt32)
+    node = getNode(ttree, node_id)
+    #Write minimum value to output array and increase index 
     values_out[out_idx] = getVal(node)
-    out_idx += 2
-    leaf_id = getLeafID(node)
-    leaf = getLeaf(ttree, leaf_id)
-    if leaf.top_id === leaf.last_id
-        setLeaf!(ttree, leaf, leaf_id) = Leaf(
-            typemax(T),
-            leaf_id,
-            leaf_id
-        )
+    out_idx += 1
+    #Repair tree 
+    top_id = getTopID(node)
+    last_id = getLastID(node)
+    bin_id = getBinID(node)
+    if top_id > last_id
+        top_id += one(I)
+        setNode!(ttree, 
+                TTreeNode(
+                typemax(T),
+                bin_id,
+                top_id,
+                last_id),
+                bin_id)
     else
-        setLeaf!(ttree, leaf, leaf_id) = Leaf(
-            values_in[leaf.top_id],
-            leaf.top_id + one(I),
-            leaf.last_id
-        )
+        top_id += one(I)
+        setNode!(ttree, 
+                TTreeNode(
+                values_in[top_id],
+                bin_id,
+                top_id,
+                last_id),
+                bin_id)
+    end
+    println("node_id $node_id")
+    lower_node_id = bin_id - (bin_id%I(2) === zero(I))
+    println("lower_node_id $lower_node_id")
+    N = ttree.n_bins
+    layer_size = n
+    n = (lower_node_id >> 2)
+    while N < ttree.n_bins + ttree.n_bins - one(I)
+        println("n $n ", N + n)
+        node_a, node_b = getNode(ttree, lower_node_id), getNode(ttree, lower_node_id + one(I))
+        if getVal(node_a) <= getVal(node_b)
+            setNode!(ttree, node_a,  N + n + one(I))
+        else
+            setNode!(ttree, node_b,  N + n + one(I))
+        end
+        layer_size = layer_size >> 1
+        N += layer_size
+        n = n >> 1
     end
 end
 #=
