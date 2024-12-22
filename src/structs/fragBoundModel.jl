7798c8bf6f9643472a8f6c4886df1c2381c6ed9d
nwamsley1 <n.t.wamsley@wustl.edu>
2024-12-22 17:18:29 -0600

    test

--- src/structs/fragBoundModel.jl
+++ src/structs/fragBoundModel.jl
@@ -6,10 +6,3 @@ end
 function (fbm::FragBoundModel)(x::AbstractFloat)
     return fbm.low_mass(x), fbm.high_mass(x)
 end
-
-# Accessor functions
-function get_bounds(model::FragBoundModel, prec_mz::AbstractFloat)
-    min_mz = model.low_mass_poly(prec_mz)
-    max_mz = model.high_mass_poly(prec_mz)
-    return (min_mz, max_mz)
-end
