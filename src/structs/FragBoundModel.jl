88fae18db714004a393a9f4c640113cf3c13769e
nwamsley1 <n.t.wamsley@wustl.edu>
2025-01-28 17:28:16 -0600

    Fix case sensitivity issue with FragBoundModel.jl

--- src/structs/FragBoundModel.jl
+++
@@ -1,8 +0,0 @@
-struct FragBoundModel
-    low_mass::ImmutablePolynomial{Float32}
-    high_mass::ImmutablePolynomial{Float32}
-end
-
-function (fbm::FragBoundModel)(x::AbstractFloat)
-    return fbm.low_mass(x), fbm.high_mass(x)
-end
