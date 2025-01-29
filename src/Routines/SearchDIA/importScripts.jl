ac956f85bece66a1242459c0317d2855efadd945
nwamsley1 <n.t.wamsley@wustl.edu>
2025-01-28 20:49:01 -0600

    Rename squareQuadModel.jl to SquareQuadModel.jl to fix case sensitivity issue

--- src/Routines/SearchDIA/importScripts.jl
+++ src/Routines/SearchDIA/importScripts.jl
@@ -29,7 +29,7 @@ function importScripts()
             "RazoQuadModel.jl",
             "SplineQuadModel.jl",
             "binIsotopeRatioData.jl",
-            "squareQuadModel.jl",
+            "SquareQuadModel.jl",
         ]
     )
     println("hey")
