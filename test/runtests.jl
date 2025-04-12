da3cd9dd0a58c4741a7e46f8b26cf4645c30183e
Nathan Wamsley <wamsleynathan@gmail.com>
2025-04-12 18:37:09 -0500

    uncommented tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -101,19 +101,19 @@ if isdir(results_dir)
 end
 @testset "Pioneer.jl" begin
     println("dir ", @__DIR__)
-    #@testset "process_test_speclib" begin 
-    #    @test size(ParseSpecLib(joinpath(@__DIR__, "./../data/library_test/defaultParseEmpiricalLibParams2.json")).libdf, 1)==120
-    #end
-    #include("./UnitTests/empiricalLibTests.jl")
+    @testset "process_test_speclib" begin 
+        @test size(ParseSpecLib(joinpath(@__DIR__, "./../data/library_test/defaultParseEmpiricalLibParams2.json")).libdf, 1)==120
+    end
+    include("./UnitTests/empiricalLibTests.jl")
     @testset "process_test" begin 
         @test SearchDIA("./../data/ecoli_test/ecoli_test_params.json")===nothing
     end
     
-    #include("./UnitTests/buildDesignMatrix.jl")
-    #include("./UnitTests/isotopeSplines.jl")
-    #include("./UnitTests/matchPeaks.jl")
-    #include("./UnitTests/queryFragmentIndex.jl")
-    #include("./UnitTests/testIsotopesJun13.jl")
-    #include("./UnitTests/uniformBassisCubicSpline.jl")
-    #include("./UnitTests/proteinInference.jl")
+    include("./UnitTests/buildDesignMatrix.jl")
+    include("./UnitTests/isotopeSplines.jl")
+    include("./UnitTests/matchPeaks.jl")
+    include("./UnitTests/queryFragmentIndex.jl")
+    include("./UnitTests/testIsotopesJun13.jl")
+    include("./UnitTests/uniformBassisCubicSpline.jl")
+    include("./UnitTests/proteinInference.jl")
 end
