a76a70a752455cab4be71ab742f159773345a041
DennisGoldfarb <goldfarb.dennis@wustl.edu>
2025-04-23 19:53:22 -0500

    added a fixed seed to library building to remove variability. In the future it should be settable from the config file.

--- src/Routines/BuildSpecLib.jl
+++ src/Routines/BuildSpecLib.jl
@@ -21,6 +21,7 @@ Output:
 function BuildSpecLib(params_path::String)
     # Clean up any old file handlers in case the program crashed
     GC.gc()
+    Random.seed!(1844)
     # Initialize timing dictionary for performance tracking
     #params_path = "/Users/n.t.wamsley/RIS_temp/koina_testing/config.json"
     timings = Dict{String, Any}()
