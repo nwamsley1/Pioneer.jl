a37df543efa38e52f3d7d6e7d55f68e0ce8b314d
nwamsley1 <78377271+nwamsley1@users.noreply.github.com>
2025-05-30 08:41:57 -0500

    init commit. Protein inference needs to distinguish between original and entrapment sequences when there are entrapment sequences in the library

--- src/utils/writeArrow.jl
+++ src/utils/writeArrow.jl
@@ -1,3 +1,4 @@
+#=
 function writeArrow(fpath::String, df::AbstractDataFrame)
     fpath = normpath(fpath)
     if Sys.iswindows()
@@ -8,6 +9,67 @@ function writeArrow(fpath::String, df::AbstractDataFrame)
         end
         mv(tpath, fpath, force = true)
     else     #If Linux/MacOS easy
+        Arrow.write(fpath, df)
+    end
+    return nothing
+end
+=#
+function writeArrow(fpath::String, df::AbstractDataFrame)
+    fpath = normpath(fpath)
+    if Sys.iswindows()
+        # Create a unique temporary file
+        tpath = tempname() * ".arrow"
+        
+        # Write to the temporary file
+        Arrow.write(tpath, df)
+        
+        # Try to delete the existing file with retries
+        if isfile(fpath)
+            max_retries = 5
+            for i in 1:max_retries
+                try
+                    # Force garbage collection to release any file handles
+                    GC.gc()
+                    
+                    # Try to delete using Julia's rm with force flag
+                    rm(fpath, force=true)
+                    break
+                catch e
+                    if i == max_retries
+                        # If all retries failed, try Windows-specific deletion
+                        try
+                            run(`cmd /c del /f /q "$fpath"`)
+                        catch
+                            # If that also fails, rename the old file instead of deleting
+                            backup_path = fpath * ".backup_" * string(time_ns())
+                            try
+                                mv(fpath, backup_path, force=true)
+                            catch
+                                error("Unable to remove or rename existing file: $fpath")
+                            end
+                        end
+                    else
+                        # Wait a bit before retrying
+                        sleep(0.1 * i)
+                    end
+                end
+            end
+        end
+        
+        # Move the temporary file to the final location
+        try
+            mv(tpath, fpath, force=true)
+        catch e
+            # If move fails, try copy and delete
+            try
+                cp(tpath, fpath, force=true)
+                rm(tpath, force=true)
+            catch
+                error("Unable to write to file: $fpath")
+            end
+        end
+    else
+        # For Linux/MacOS, use the simple approach
         Arrow.write(fpath, df)
     end
     return nothing
