4fce531a9b233fe08ba5ce67da088fba4493c661
DennisGoldfarb <goldfarb.dennis@wustl.edu>
2025-06-26 00:24:36 -0500

    Updated probit loss. Converges faster.

--- src/utils/ML/probitRegression.jl
+++ src/utils/ML/probitRegression.jl
@@ -14,7 +14,7 @@ function fillZandW!(Z::Vector{T}, W::Vector{T}, η::Vector{T}, y::Vector{Bool},d
                     η[i] = log(μ)
                 else
                     Z[i] = η[i] - μ/ϕ
-                    η[i] = 1- log(μ)
+                    η[i] = log1p(-μ)
                 end
                 W[i] = (ϕ^2)/(μ*(1 - μ))
             end
