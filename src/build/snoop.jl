77dc551487e71129b0159b6fd932220ccd7a55cb
DennisGoldfarb <d.goldfarb@wustl.edu>
2025-07-21 06:56:55 -0500

    snoop

--- src/build/snoop.jl
+++ src/build/snoop.jl
@@ -16,10 +16,23 @@ GetSearchParams(
 # Parse example empirical library to compile parsing code
 ParseSpecLib(joinpath(data_dir, "library_test", "defaultParseEmpiricalLibParams2.json"))
 
-# Execute a small search to precompile search routines
+
+# Build a tiny Prosit library and search it with low memory thresholds
+BuildSpecLib(joinpath(data_dir, "precompile", "build_ecoli_prosit.json"))
+#SearchDIA(joinpath(data_dir, "precompile", "search_ecoli_prosit.json"))
+
+# Build a tiny Altimeter library
+#BuildSpecLib(joinpath(data_dir, "precompile", "build_ecoli_prosit.json"))
+
+# Execute a small Altimeter search
 SearchDIA(joinpath(data_dir, "ecoli_test", "ecoli_test_params.json"))
 
 
 
-#BuildSpecLib("build/buildspeclib_params.json")
+
+
+
+
+
+
 #convertMzML("data/mzML/")
