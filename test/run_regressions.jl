96f01ef8e2a0366afa56336a837f10bac86812ef
DennisGoldfarb <d.goldfarb@wustl.edu>
2025-11-21 12:25:22 -0600

    get params from github repo

--- test/run_regressions.jl
+++ test/run_regressions.jl
@@ -1,6 +1,10 @@
 using Pioneer
 
-param_dir = "C:/pioneer-runner/params" 
+param_dir = get(
+    ENV,
+    "PIONEER_PARAMS_DIR",
+    joinpath(@__DIR__, "..", "pioneer-regression-configs", "params"),
+)
 
 for path in readdir(param_dir; join=true)
     endswith(path, ".json") || continue
