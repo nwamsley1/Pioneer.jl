2fddef90256a9add2040c8c9d547598ca392466d
DennisGoldfarb <d.goldfarb@wustl.edu>
2026-02-14 20:05:45 -0600

    hardcode q 0.02

--- src/Routines/SearchDIA/SearchMethods/FirstPassSearch/getBestPrecursorsAccrossRuns.jl
+++ src/Routines/SearchDIA/SearchMethods/FirstPassSearch/getBestPrecursorsAccrossRuns.jl
@@ -239,7 +239,7 @@ function get_best_precursors_accross_runs(
 
     passing_precursors = Set{UInt32}(
         precursor_ids[i] for i in eachindex(precursor_ids)
-        if global_qvals[i] <= 0.01
+        if global_qvals[i] <= 0.02
     )
 
     @info "Global precursor q-value filter summary" max_q_val=max_q_val n_total=length(precursor_ids) n_passing=length(passing_precursors)
