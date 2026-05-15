6351d96ffdcb73e83c6a5371b7354598fc31938b
DennisGoldfarb <d.goldfarb@wustl.edu>
2026-05-15 17:39:09 -0500

    Add separate chromatogram trace integration support

--- test/runtests_part3_units.jl
+++ test/runtests_part3_units.jl
@@ -35,6 +35,7 @@ include("./UnitTests/test_fused_prec_filters.jl")
 include("./UnitTests/test_run_fused.jl")
 include("./UnitTests/test_sort_detailed_fragments.jl")
 include("./UnitTests/test_feature_finiteness.jl")
+include("./UnitTests/test_chromatogram_integration_trace_order.jl")
 
 # FileOperations focused tests
 include("./utils/FileOperations/io/test_arrow_operations_basic.jl")
