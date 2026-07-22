8d15fe97f955317955b2bbdba171cdf2d54d4345
DennisGoldfarb <d.goldfarb@wustl.edu>
2026-07-22 07:11:17 -0500

    Use shared semi-supervised iteration limit for global scoring

--- test/Routines/SearchDIA/SearchMethods/PrecursorScoringSearch/test_global_precursor_scoring.jl
+++ test/Routines/SearchDIA/SearchMethods/PrecursorScoringSearch/test_global_precursor_scoring.jl
@@ -183,11 +183,12 @@
             lgbm_hp = test_hyperparameters,
             min_training_class_count = 1,
             max_train = 1_000,
+            max_iterations = 3,
         )
 
         @test length(scored.scores) == nrow(table)
         @test length(scored.models) == 2
-        @test scored.iter == 1
+        @test scored.iter in 1:3
         @test mean(scored.scores[target]) > mean(scored.scores[.!target])
     end
 end
